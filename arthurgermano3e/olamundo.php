f06d9de64b1647baeebcca5f31a3ca0e4bb0cc2e
Arthur Germano <162514571+ArthurGerman@users.noreply.github.com>
2024-07-22 18:17:56 -0300

    Add files via upload

---
+++ arthurgermano3e/olamundo.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Minha primeira página html com PHP</h1>
+    <?php
+        echo "Olá mundo !";
+    ?>
+</body>
+</html>
