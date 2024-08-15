c35023f39c3ee2ade867c72b5a2327af59119035
Arthur Germano <162514571+ArthurGerman@users.noreply.github.com>
2024-08-15 14:52:32 -0300

    Add files via upload

---
+++ arthurgermano3e/aula09 - transferencia de dados/aula09 - transferencia de dados.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="calcular.php" method="post">
+
+        <label for="n1">Número 1:</label>
+        <input type="number" name="n1"><br> <!-- Primeiro campo -->
+
+        <label for="n2">Número 2:</label> <!-- Primeiro campo -->
+        <input type="number" name="n2"><br>
+
+        <input type="submit" value="Somar"><br> <!-- Botão de soma -->
+
+        Resultado: <input type="text" width="5"><br>
+
+    </form>
+</body>
+</html>
---
+++ arthurgermano3e/aula09 - transferencia de dados/calcular.php
@@ -0,0 +1,7 @@
+<?php 
+    $n1 = $_POST["n1"];
+    $n2 = $_POST["n2"];
+
+    $resultado = $n1 + $n2;
+    echo "A soma é: ",$resultado;
+?>
